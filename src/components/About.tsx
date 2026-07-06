ad49b506d75fb33f56c5d2743932f02014bc71c2
lovedosegaming-design <lovedosegaming@gmail.com>
2026-07-06 21:58:19 +0530

    Update skills layout: Premiere Pro 95%, Sound Design 100%, Colour Grading 100%, After Effects 50%, Photoshop 50%

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,10 +5,10 @@ import editorPhoto from '../editor-photo.jpg';
 
 const skills = [
   { name: 'Premiere Pro', level: 95 },
-  { name: 'After Effects', level: 85 },
-  { name: 'Photoshop', level: 80 },
-  { name: 'Sound Design', level: 75 },
+  { name: 'Sound Design', level: 100 },
   { name: 'Colour Grading', level: 100 },
+  { name: 'After Effects', level: 50 },
+  { name: 'Photoshop', level: 50 },
 ];
 
 export default function About() {
